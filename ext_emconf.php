0ddedcfd1f7cb503fb31aa1163fd7eee25f9af52
Sven K <sven@skom.de>
2020-03-04 15:27:22 +0530

    Dependency from skom_sitepackage

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,6 +12,7 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '8.7.0-9.5.99',
             'rte_ckeditor' => '8.7.0-9.5.99',
             'bootstrap_package' => '10.0.0-11.0.99',
+            'skom_sitepackage' => '1.1.0-1.99.99',
             'cart' => '6.7.0-7.0.0',
             'cart_books' => '2.0.3-2.5.99'
         ],
